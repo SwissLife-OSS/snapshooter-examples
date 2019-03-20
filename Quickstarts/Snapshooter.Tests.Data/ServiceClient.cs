ca358b9b57eb6cb2f7f72ed4a5dbbfadd23f2f74
Normen Scheiber <Normen.Scheiber@swisslife.ch>
2019-03-20 16:23:53 +0100

    Added simple snapshot test examples and snapshot ignore test examples.

---
+++ Quickstarts/Snapshooter.Tests.Data/ServiceClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Snapshooter.Tests.Data
+{
+    public class ServiceClient
+    {
+		public TestPerson CreatePerson(Guid id, string firstName, string lastName)
+        {
+            return TestPersonBuilder.Create()
+                .WithId(id)
+                .WithFirstname(firstName)
+                .WithLastname(lastName)
+                .Build();
+        }
+
+        public async Task<TestPerson> CreatePersonAsync(Guid id, string firstName, string lastName)
+        {
+            await Task.Delay(10);
+
+            TestPerson person = TestPersonBuilder.Create()
+                .WithId(id)
+                .WithFirstname(firstName)
+                .WithLastname(lastName)
+                .Build();
+
+            await Task.Delay(10);
+
+            return person;
+        }
+
+        public TestPerson CreatePerson(string firstName, string lastName)
+        {
+            return TestPersonBuilder.Create()
+                .WithId(Guid.NewGuid())
+                .WithFirstname(firstName)
+                .WithLastname(lastName)
+                .Build();
+        }
+    }
+}
